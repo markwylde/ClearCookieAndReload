df52df1bf24392d59750ab86a151a308983a7c88
Mark Wylde <me@markwylde.com>
2020-08-21 07:07:15 +1000

    Add clearing localStorage and reload page

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# ClearCookieAndReload
+# chrome-clear-and-reload
 
-[Google chrome extension](https://chrome.google.com/webstore/detail/clear-cookie-and-reload/clinijjpaohndgmfepgpifcfnmlecbom) to clear cookie for current tab and reload
-
-## Intended Use
-Helpful for developers who need to clear all identifying cookies to test a clean environment.
-Watch free match on [starsports.com](http://www.starsports.com/) using this app, click on the app when 10 minutes preview time gets over. Enjoy :)
+A chrome/brave extension to clear localStorage and cookies from the current active tab.
--- background.js
+++ background.js
@@ -1,36 +1,43 @@
+/* global chrome */
+
 // triggered when user clicks on installed extention icon
 chrome.browserAction.onClicked.addListener(function (tab) {
+  const url = tab.url;
 
-    let url = tab.url;
+  // clear all localstorage
+  chrome.tabs.executeScript(tab.id, { code: 'localStorage.clear()' });
 
-    // clear all cookies for the current url
-    chrome.cookies.getAll({ url: url }, function (cookies) {
-        console.log("Clearing cookies for active urL", url, cookies);
-        clearCookies(cookies);
-    });
+  // clear all cookies for the current url
+  chrome.cookies.getAll({ url: url }, function (cookies) {
+    console.log('Clearing cookies for active url', url, cookies);
+    clearCookies(cookies);
+  });
 
-    // retrive domain from active tab
-    let matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
-    let domain = matches && matches[1].replace('www.', '');
-    domain = "." + domain;
+  // retrive domain from active tab
+  const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+  let domain = matches && matches[1].replace('www.', '');
+  domain = '.' + domain;
 
-    // get all cookies for domain
-    chrome.cookies.getAll({ domain: domain }, function (cookies) {
-        console.log("Clearing cookies for domain", domain, cookies);
-        clearCookies(cookies);
-    });
+  // get all cookies for domain
+  chrome.cookies.getAll({ domain: domain }, function (cookies) {
+    console.log('Clearing cookies for domain', domain, cookies);
+    clearCookies(cookies);
+  });
+
+  // reload tab
+  chrome.tabs.reload(tab.id);
 });
 
-function clearCookies(cookies) {
-    // iterate on cookie to get cookie detail
-    for (let i = 0; i < cookies.length; i++) {
-        let url = "http" + (cookies[i].secure ? "s" : "") + "://" + cookies[i].domain + cookies[i].path;
-        let cname = cookies[i].name;
+function clearCookies (cookies) {
+  // iterate on cookie to get cookie detail
+  for (let i = 0; i < cookies.length; i++) {
+    const url = 'http' + (cookies[i].secure ? 's' : '') + '://' + cookies[i].domain + cookies[i].path;
+    const cname = cookies[i].name;
 
-        // delete cookie
-        chrome.cookies.remove({
-            url: url,
-            name: cname
-        });
-    }
+    // delete cookie
+    chrome.cookies.remove({
+      url: url,
+      name: cname
+    });
+  }
 }
